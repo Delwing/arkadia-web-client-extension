ccf3db1e3c4dfbcf5fbfbe5d97f0974750db9df3
Piotr <delwing@gmail.com>
2025-07-22 00:51:32 +0200

    Fix web-client build

--- web-client/vite.config.ts
+++ web-client/vite.config.ts
@@ -1,13 +1,13 @@
-import {defineConfig} from 'vite'
+import {defineConfig, type PluginOption} from 'vite'
 import react from '@vitejs/plugin-react'
 import {resolve} from "path";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
     plugins: [
-        react(), 
+        react(),
         tsconfigPaths()
-    ],
+    ] as PluginOption[],
     base: "./",
     build: {
         minify: true,
