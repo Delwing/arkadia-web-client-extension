855d008090b0d0e406c46d41941771c110e7987d
Piotr <delwing@gmail.com>
2025-07-22 12:52:06 +0200

    feat: color entire disappearing magic line

--- client/src/scripts/magikZnika.ts
+++ client/src/scripts/magikZnika.ts
@@ -1,18 +1,17 @@
 import Client from "../Client";
-import { colorString, colorStringInLine, findClosestColor } from "../Colors";
+import { colorString, findClosestColor } from "../Colors";
 
 export default function initMagikZnika(client: Client) {
     const COLOR = findClosestColor("#ff6347");
     const tag = "magik-znika";
-    const prefix = "\n\t" + colorString("[  MAGIK ZNIKA   ] ", COLOR);
+    const prefix = "\n\t" + colorString("[  MAGIK ZNIKA   ]", COLOR);
 
-    const format = (line: string) => `${client.prefix(line, prefix)}\n`;
+    const format = (line: string) => `${prefix}\n${colorString(line, COLOR)}\n`;
 
     client.Triggers.registerTrigger(
         /^Bialy, zimny plomien ogarnia (.*), w kilka chwil spopielajac .* calkowicie\.$/,
-        (_raw, line, matches) => {
-            const colored = colorStringInLine(line, matches[1], COLOR);
-            return format(colored);
+        (_raw, line) => {
+            return format(line);
         },
         tag
     );
