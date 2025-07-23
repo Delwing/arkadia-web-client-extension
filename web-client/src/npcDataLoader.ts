e6f6cde8dbe16949e0fbdd82b147a5304bc82aa4
Piotr <delwing@gmail.com>
2025-07-23 23:39:16 +0200

    Store new NPCs in IndexedDB (#536)

--- web-client/src/npcDataLoader.ts
+++ web-client/src/npcDataLoader.ts
@@ -6,7 +6,6 @@ const TTL = 24 * 60 * 60 * 1000; // 24h
 export function loadNpcData() {
     return loadCachedJSON({
         url: 'https://delwing.github.io/arkadia-mapa/data/npc.json',
-        localStorageKey: 'npc',
         indexedDB: { dbName: 'ArkadiaNpcDB', storeName: 'npcData', key: 'npc' },
         ttl: TTL,
     });
