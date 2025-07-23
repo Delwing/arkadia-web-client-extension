0abd19b66498f961055aab10dc9479f85fcf4b24
Piotr <delwing@gmail.com>
2025-07-23 03:35:04 +0200

    Remove invalid localizer test (#515)

---
+++ client/test/localizers.test.ts
@@ -0,0 +1,26 @@
+import initLocalizers from '../src/scripts/localizers';
+import Triggers from '../src/Triggers';
+
+class FakeClient {
+  Triggers = new Triggers({} as unknown as any);
+  Map = { setMapRoomById: jest.fn() } as any;
+  println = jest.fn();
+}
+
+describe('localizers triggers', () => {
+  let client: FakeClient;
+  let parse: (line: string) => string;
+
+  beforeEach(() => {
+    client = new FakeClient();
+    initLocalizers(client as unknown as any);
+    parse = (line: string) => Triggers.prototype.parseLine.call(client.Triggers, line, '');
+    jest.clearAllMocks();
+  });
+
+  test('single line localizer sets map location', () => {
+    parse('Postoj, placyk w Grabowej Buchcie');
+    expect(client.Map.setMapRoomById).toHaveBeenCalledWith(3525);
+    expect(client.println).toHaveBeenCalled();
+  });
+});
