85bdda0249dd07da47d175ed8b3d9bc0ee19c723
Piotr <delwing@gmail.com>
2025-07-24 14:23:40 +0200

    fix(build): correct regex syntax (#550)

--- client/src/scripts/newMail.ts
+++ client/src/scripts/newMail.ts
@@ -5,7 +5,7 @@ export default function initNewMail(client: Client) {
     const COLOR = findClosestColor("tomato");
     const tag = "new-mail";
     const prefix = colorString("[ POCZTA ] ", COLOR);
-    const pattern = /^Masz nowa poczte od (?'sender'[A-Za-z]+)\.$/;
+    const pattern = /^Masz nowa poczte od (?<sender>[A-Za-z]+)\.$/;
 
     const format = (line: string) => `\n\n${client.prefix(colorString(line, COLOR), prefix)}\n\n`;
 
