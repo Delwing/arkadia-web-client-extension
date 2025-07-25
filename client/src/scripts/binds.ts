5a1830bc626550599535c220aa64ef91fb3a0376
Piotr <delwing@gmail.com>
2025-07-26 01:39:25 +0200

    Add support bind (#568)

--- client/src/scripts/binds.ts
+++ client/src/scripts/binds.ts
@@ -5,9 +5,11 @@ export default function initBinds(client: Client, aliases?: { pattern: RegExp; c
     function printBinds() {
         const main = client.FunctionalBind.getLabel();
         const lamp = formatLabel(client.lampBind);
+        const support = formatLabel(client.supportBind);
         const lines = [
             `Domy\u015Blny: ${main}`,
             `Nape\u0142nij lamp\u0119: ${lamp}`,
+            `Wesprzyj: ${support}`,
         ];
         client.println(lines.join("\n"));
     }
