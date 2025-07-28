6cf8b0e1b6c5bc3d57d06205fa4922f30e457f7e
Piotr <delwing@gmail.com>
2025-07-29 01:58:18 +0200

    Add additional tests for web client

---
+++ web-client/src/mapDataLoader.test.ts
@@ -0,0 +1,33 @@
+import { loadMapData, loadColors } from './mapDataLoader';
+import { loadCachedJSON } from '@client/src/utils/dataCache.ts';
+
+jest.mock('@client/src/utils/dataCache.ts', () => ({
+  loadCachedJSON: jest.fn()
+}));
+
+describe('mapDataLoader', () => {
+  beforeEach(() => {
+    (loadCachedJSON as jest.Mock).mockClear();
+  });
+
+  test('loadMapData passes correct options', () => {
+    const onProgress = jest.fn();
+    loadMapData(onProgress);
+    expect(loadCachedJSON).toHaveBeenCalledWith({
+      url: 'https://delwing.github.io/arkadia-mapa/data/mapExport.json',
+      localStorageKey: 'cachedMapData',
+      indexedDB: { dbName: 'ArkadiaMapDB', storeName: 'mapData', key: 'mapExport' },
+      ttl: 86400000,
+      onProgress
+    });
+  });
+
+  test('loadColors passes correct options', () => {
+    loadColors();
+    expect(loadCachedJSON).toHaveBeenCalledWith({
+      url: 'https://delwing.github.io/arkadia-mapa/data/colors.json',
+      localStorageKey: 'cachedColors',
+      ttl: 86400000
+    });
+  });
+});
