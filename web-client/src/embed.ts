27fc564bc58273bbb4300ec4284e25c40f3320a9
Piotr Wilczyński <delwing@gmail.com>
2025-07-28 22:44:59 +0200

    fix room highlight

--- web-client/src/embed.ts
+++ web-client/src/embed.ts
@@ -158,7 +158,7 @@ export default class EmbeddedMap {
 
             this.highlights.forEach(highlight => {
                 const room = this.reader.getRoomById(highlight)
-                if (room.render && room.x && room.y) {
+                if (room.render && this.renderer.area.getRoomById(highlight)) {
                     this.renderer.renderHighlight(highlight);
                 }
             })
