d39291e57fd40f05cb73035cd6db7233b54770a8
Piotr <delwing@gmail.com>
2025-08-05 16:00:33 +0200

    Color coin amounts listed without 'monet'

--- client/src/scripts/coinColors.ts
+++ client/src/scripts/coinColors.ts
@@ -6,13 +6,12 @@ export default function initCoinColors(client: Client) {
     const tag = "coinColors";
     const patterns: { regex: RegExp; color: number }[] = [
         { regex: /(\w+\s+)?zlot(a|e|ych) monet(y|e|a|)/i, color: GOLD_COLOR },
-        { regex: /[Zz]lot(a|e|ych) monet(y|e|a|)/, color: GOLD_COLOR },
         { regex: /(\w+\s+)?mithrylow(a|e|ych) monet(y|e|a|)/i, color: MITHRIL_COLOR },
-        { regex: /[Mm]ithrylow(a|e|ych) monet(y|e|a|)/, color: MITHRIL_COLOR },
         { regex: /(\w+\s+)?srebrn(a|e|ych) monet(y|e|a|)/i, color: SILVER_COLOR },
-        { regex: /srebrn(a|e|ych) monet(y|e|a|)/i, color: SILVER_COLOR },
         { regex: /(\w+\s+)?miedzian(a|e|ych) monet(y|e|a|)/i, color: COPPER_COLOR },
-        { regex: /miedzian(a|e|ych) monet(y|e|a|)/i, color: COPPER_COLOR }
+        { regex: /(\w+\s+)?zlot(a|e|ych)(?!\s+monet)/i, color: GOLD_COLOR },
+        { regex: /(\w+\s+)?srebrn(a|e|ych)(?!\s+monet)/i, color: SILVER_COLOR },
+        { regex: /(\w+\s+)?miedzian(a|e|ych)(?!\s+monet)/i, color: COPPER_COLOR }
     ];
     patterns.forEach(({ regex, color }) => {
         client.Triggers.registerTrigger(regex, (raw, _line, m) => {
