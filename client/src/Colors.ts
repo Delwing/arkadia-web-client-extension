ee479eefd7025da441c9f5537b2db9731fdc7afa
Piotr <delwing@gmail.com>
2025-08-08 02:54:04 +0200

    chore: remove xterm palette debug log (#684)

--- client/src/Colors.ts
+++ client/src/Colors.ts
@@ -32,7 +32,6 @@ const palette = (() => {
 colorCodes.xterm = palette === 'proper' ? colorCodes.xtermProper : colorCodes.xtermArkadia;
 
 export function setXtermPalette(p: 'arkadia' | 'proper') {
-    console.log('Setting xterm palette to', p);
     colorCodes.xterm = p === 'proper' ? colorCodes.xtermProper : colorCodes.xtermArkadia;
 }
 
