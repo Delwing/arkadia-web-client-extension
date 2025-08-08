e0c29a32f0c2df8c4ffafd260390c5e628576a06
Piotr <delwing@gmail.com>
2025-08-08 14:26:08 +0200

    Add daje-ci highlight script (#689)

---
+++ client/src/scripts/dajeCiHighlight.ts
@@ -0,0 +1,14 @@
+import Client from "../Client";
+import { colorStringInLine, findClosestColor } from "../Colors";
+
+export default function initDajeCiHighlight(client: Client) {
+    const TURQUOISE = findClosestColor("#40e0d0");
+    const pattern = /^(?:[ >]*[A-Za-z !()]+ daje ci )(.*)$/;
+    client.Triggers.registerTrigger(pattern, (raw, _line, matches) => {
+        const group = matches[1];
+        if (group !== "nowy zapal do walki.") {
+            const start = matches.index ?? 0;
+            return colorStringInLine(raw, group, TURQUOISE, start);
+        }
+    }, "daje-ci-highlight");
+}
