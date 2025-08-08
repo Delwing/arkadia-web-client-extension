0fe982c75dd1f5580a4a083e1737c789afd3aae9
Piotr Wilczyński <delwing@gmail.com>
2025-08-08 03:02:35 +0200

    change way space is represented

--- web-client/src/FightTitle.ts
+++ web-client/src/FightTitle.ts
@@ -6,7 +6,7 @@ export default class FightTitle {
   private playerNum?: string;
   private isFighting = false;
   private readonly fightPrefix = "⚔ ";
-  private readonly idlePrefix = "\u00A0\u00A0"; // two non-breaking spaces
+  private readonly idlePrefix = "ㅤ ";
   private enabled = true;
 
   constructor(client: typeof ArkadiaClient) {
