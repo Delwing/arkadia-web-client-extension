f59b082e15f3a8f50bab6ba29b941b87d149cbe7
Piotr <delwing@gmail.com>
2025-08-08 02:43:52 +0200

    Reset fight icon on disconnect

--- web-client/src/FightTitle.ts
+++ web-client/src/FightTitle.ts
@@ -5,12 +5,16 @@ export default class FightTitle {
   private client: typeof ArkadiaClient;
   private playerNum?: string;
   private isFighting = false;
+  private readonly fightPrefix = "⚔ ";
+  private readonly idlePrefix = "  ";
 
   constructor(client: typeof ArkadiaClient) {
     this.client = client;
     this.baseTitle = document.title;
+    this.updateTitle(false, true);
     client.on("gmcp.char.info", (info: any) => this.handleCharInfo(info));
     client.on("gmcp.objects.data", (data: Record<string, any>) => this.handleObjectsData(data));
+    client.on("client.disconnect", () => this.reset());
   }
 
   private handleCharInfo(info: any) {
@@ -28,10 +32,16 @@ export default class FightTitle {
     this.updateTitle(fighting);
   }
 
-  private updateTitle(fighting: boolean) {
-    if (this.isFighting === fighting) return;
+  private reset() {
+    this.playerNum = undefined;
+    this.updateTitle(false, true);
+  }
+
+  private updateTitle(fighting: boolean, force = false) {
+    if (!force && this.isFighting === fighting) return;
     this.isFighting = fighting;
-    document.title = this.isFighting ? `⚔ ${this.baseTitle}` : this.baseTitle;
+    const prefix = this.isFighting ? this.fightPrefix : this.idlePrefix;
+    document.title = `${prefix}${this.baseTitle}`;
   }
 }
 
