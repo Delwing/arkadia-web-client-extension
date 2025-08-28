51621b3c1654f961ed6ac0e27c158a4dd7c6ef2a
Piotr <delwing@gmail.com>
2025-08-28 10:35:06 +0200

    refactor: export settings type for vite (#768)

--- web-client/src/options/defaultSettings.ts
+++ web-client/src/options/defaultSettings.ts
@@ -1,1 +1,2 @@
-export { Settings, defaultSettings } from '@client/src/defaultSettings';
+export { defaultSettings } from '@client/src/defaultSettings';
+export type { Settings } from '@client/src/defaultSettings';
