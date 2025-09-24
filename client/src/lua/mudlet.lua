da83cbbe2d2ec9921505956412f3fd1c761bb9d8
Piotr Wilczyński <delwing@gmail.com>
2025-09-24 20:30:40 +0200

    add magic_prefix function to lua

--- client/src/lua/mudlet.lua
+++ client/src/lua/mudlet.lua
@@ -1,3 +1,12 @@
 function string:starts(prefix)
     return string.sub(self, 1, string.len(prefix)) == prefix
 end
+
+function magic_prefix(text)
+    text = string.format("[ %s ] ", text)
+    prefix(text)
+    if selectString(text, 1) > -1 then
+        fg("orange")
+    end
+    resetFormat()
+end
