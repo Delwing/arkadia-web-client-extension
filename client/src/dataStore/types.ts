bc87483d9b5130ae429c9683a071b1cb7de2706b
Piotr <delwing@gmail.com>
2025-10-24 18:49:16 +0200

    Add shared data store infrastructure

---
+++ client/src/dataStore/types.ts
@@ -0,0 +1,33 @@
+export interface RefreshMetadata {
+  refreshedAt: number;
+  hash?: string;
+}
+
+export interface LoaderContext<TSnapshot, TMeta extends RefreshMetadata> {
+  previousSnapshot?: TSnapshot;
+  metadata?: TMeta;
+  force?: boolean;
+}
+
+export interface LoaderResult<TSnapshot, TMeta extends RefreshMetadata> {
+  snapshot: TSnapshot;
+  metadata?: Partial<TMeta>;
+}
+
+export interface LoaderStrategy<TSnapshot, TMeta extends RefreshMetadata = RefreshMetadata> {
+  load(context: LoaderContext<TSnapshot, TMeta>): Promise<LoaderResult<TSnapshot, TMeta>>;
+}
+
+export interface StorageStrategy<TSnapshot, TMeta extends RefreshMetadata = RefreshMetadata> {
+  readSnapshot(): Promise<TSnapshot | undefined>;
+  writeSnapshot(snapshot: TSnapshot | undefined): Promise<void>;
+  readMetadata(): Promise<TMeta | undefined>;
+  writeMetadata(metadata: TMeta | undefined): Promise<void>;
+  clear(): Promise<void>;
+}
+
+export interface SubscriptionOptions {
+  emitInitial?: boolean;
+}
+
+export type SnapshotListener<TSnapshot> = (snapshot: TSnapshot | undefined) => void;
