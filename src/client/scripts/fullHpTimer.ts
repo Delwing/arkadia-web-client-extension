46bc7b1e847ee24baa58008bc854b991f79b745e
Piotr Wilczynski <delwing@gmail.com>
2025-11-09 13:37:38 +0100

    fix full hp timer

--- src/client/scripts/fullHpTimer.ts
+++ src/client/scripts/fullHpTimer.ts
@@ -21,7 +21,7 @@ export default function initFullHpTimer(client: Client) {
         timer = window.setTimeout(() => {
             const plain = "Jestes w pelni zdrowia.";
             const msg = colorString(plain, SPRING_GREEN);
-            client.println(`\n${msg}\n`);
+            client.println(msg);
             client.notify(plain);
             client.sendEvent("notify", { text: plain });
             timer = null;
