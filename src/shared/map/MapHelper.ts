898e62f3a3f14f6bc11556d3b8eac2a2fe5a6afc
Piotr Wilczyński <delwing@gmail.com>
2026-02-02 13:12:39 +0100

    map helper will evaluate special exits first when doing positional lookup of room by exits

--- src/shared/map/MapHelper.ts
+++ src/shared/map/MapHelper.ts
@@ -296,11 +296,22 @@ export default class MapHelper {
                 this.currentRoom.specialExits ?? {}
             );
             const potentialExit = getLongDir(direction);
-            if (!this.currentRoom.exits || !this.currentRoom.exits[potentialExit]) {
+            if (this.currentRoom.specialExits) {
+                const specialExitMatch = Object.entries(this.currentRoom.specialExits)
+                    .filter(([_, id]) => {
+                        const target = this.mapReader.getRoom(id);
+                        return this.findRoomByExit(this.currentRoom, target, potentialExit);
+                    })
+                    .map(([exit]) => exit);
+                if (specialExitMatch.length > 0) {
+                    actualDirection = getShortDir(specialExitMatch[0]);
+                }
+            }
+            if (actualDirection === direction && (!this.currentRoom.exits || !this.currentRoom.exits[potentialExit])) {
                 const exits = Object.entries(allExits)
                     .filter(([_, id]) => {
                         const target = this.mapReader.getRoom(id);
-                        return this.findRoomByExit(this.currentRoom, target, getLongDir(direction));
+                        return this.findRoomByExit(this.currentRoom, target, potentialExit);
                     })
                     .map(([exit]) => exit);
                 if (exits.length > 0) {
