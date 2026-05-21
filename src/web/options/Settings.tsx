108382a0a2be6e20ef4d9afcf4a32ca845c03e3a
Piotr Wasilewski <87piwa@gmail.com>
2026-05-21 12:29:32 +0200

    adjust reset button height to 30px with original padding

--- src/web/options/Settings.tsx
+++ src/web/options/Settings.tsx
@@ -297,7 +297,7 @@ function SettingsForm({registerSave}: { registerSave: (cb: (sharedSettings: Sett
                                                 s.shortExitsBackgroundColor = 'transparent';
                                             })}
                                             title="Przywróć wszystkie domyślne ustawienia"
-                                            style={{ padding: "0px 0.5rem", height: "21px", lineHeight: "21px", fontSize: "0.875rem" }}
+                                            style={{ padding: "0.25rem 0.5rem", height: "30px", lineHeight: "30px" }}
                                         >
                                             Przywróć domyślne
                                         </button>
